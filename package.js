f1273ba01a334fe578e2c98d674c2c6d0f02e8a1
Tom Connors <tvconnors@gmail.com>
2013-09-16 10:52:26 -0400

    added pacakge.js

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  summary: "jwerty packged for meteor"
+});
+
+Package.on_use(function (api) {
+
+  api.add_files([
+    'lib/jwerty/jwerty.js'
+    ], ['client']
+  );
+
+  api.export(["jwerty"], ["client"]);
+  
+});
